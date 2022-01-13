a359ca170daac7d8da28e4758e5f9fc9ea6f3da6
Grecia RO <56781403+greciaro@users.noreply.github.com>
2022-01-13 10:23:56 -0800

    Add files via upload

--- limit_swith_servo_motor_arduino.ino
+++ limit_swith_servo_motor_arduino.ino
@@ -4,40 +4,45 @@ Servo motor; //create servo object to control a servo
 
 
 
-int Lswitch = 2;
-int pos = 0; //variable to store the motor position
+//int Lswitch = 2;
+//int pos = 0; //variable to store the motor position
 
 
 
 void setup() {
   Serial.begin(9600);  //This value of bits per second is by default
-  pinMode(Lswitch,INPUT);
-  motor.attach(9); //attaches the servo on pin 9 to the servo object
+  //pinMode(Lswitch,INPUT);
+  motor.attach(7); //attaches the servo on pin 9 to the servo object
   //pinMode(motor,OUTPUT);
 }
 
 void loop() {
 
-  if (digitalRead(Lswitch)== LOW){
-    //Serial.println("door is innactive");
-  }
-
-  if (digitalRead(Lswitch)== HIGH){
-    Serial.println("door is active");
+  servo.detach(); // This indicates the motor to stop
+  delay(2000);  // The motor will remained stopped for 2 seconds 
+  servo.attach(7);  //This re-attach the servo to pin #7
   
-    for (pos = 0; pos <= 180; pos += 1){ //goes from 0 degrees to 350 degrees
+
+  //if (digitalRead(Lswitch)== LOW){
+    //Serial.println("door is innactive");
+  //}
+
+ // if (digitalRead(Lswitch)== HIGH){
+  //  Serial.println("door is active");
+  
+  //  for (pos = 0; pos <= 180; pos += 1){ //goes from 0 degrees to 350 degrees
       //in steps of 1 degree
-      motor.write(pos); //tells servo to go to position in variable 'pos'
-      delay(15);  //waits 15ms for the servo to reach the position
-    }
+   //   motor.write(pos); //tells servo to go to position in variable 'pos'
+   //   delay(15);  //waits 15ms for the servo to reach the position
+   // }
 
     //delay(15000); //15000 = 15 seconds
     
-    for (pos = 180; pos >= 0; pos += 1){ //goes from 0 degrees to 350 degrees
-      motor.write(pos); //tells servo to go to position in variable 'pos'
-      delay(15);  //waits 15ms for the servo to reach the position    
-    }
+ //   for (pos = 180; pos >= 0; pos += 1){ //goes from 0 degrees to 350 degrees
+   //   motor.write(pos); //tells servo to go to position in variable 'pos'
+   //   delay(15);  //waits 15ms for the servo to reach the position    
+   // }
 
-  }
+ // }
 
 }
