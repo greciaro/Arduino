bb4ccc437c393e31c1c7e84ac4ce37324fe55fed
Grecia RO <56781403+greciaro@users.noreply.github.com>
2022-01-14 12:08:06 -0800

    Add files via upload

--- limit_swith_servo_motor_arduino.ino
+++ limit_swith_servo_motor_arduino.ino
@@ -2,55 +2,42 @@
 #include <Servo.h> //library to operate servo motors
 Servo motor; //create servo object to control a servo
 
-
-
-//int Lswitch = 2;
-//int pos = 0; //variable to store the motor position
-
-
+int Lswitch = 2;
+int active = 0; //variable to store if the switch was activated
+int pos = 0; //variable to store the position of the motor
 
 void setup() {
   Serial.begin(9600);  //This value of bits per second is by default
-  //pinMode(Lswitch,INPUT);
-  motor.attach(7); //attaches the servo on pin 9 to the servo object
+  motor.attach(7); //attaches the servo on pin 7 to the servo object
+
+  pinMode(Lswitch,INPUT_PULLUP);
   //pinMode(motor,OUTPUT);
 }
 
 void loop() {
-
-  servo.detach(); // telling the motor to stop
-  delay(2000);  // motor will remained stopped for 2 seconds 
-  servo.attach(7);  //re-attach the servo to pin #7
-  servo.write(180); // telling the motor to rotate continously
-  delay(5000);  // motor will continue rotating for 5 seconds
   
-  servo.detach(); // telling the motor to stop
-  delay(15000);  // motor will remained stopped for 15 seconds 
-
-  servo.attach(7);  //re-attach the servo to pin #7
-  servo.write(0); // telling the motor to rotate continously
-  delay(5000);  // motor will continue rotating for 5 seconds
+  //motor.detach();
+  //delay(1000);
   
-  //if (digitalRead(Lswitch)== LOW){
-    //Serial.println("door is innactive");
-  //}
+  if(digitalRead(Lswitch) == LOW){ //"LOW" means that the switch has been activated
+    active = 1;
+  }
 
- // if (digitalRead(Lswitch)== HIGH){
-  //  Serial.println("door is active");
-  
-  //  for (pos = 0; pos <= 180; pos += 1){ //goes from 0 degrees to 350 degrees
-      //in steps of 1 degree
-   //   motor.write(pos); //tells servo to go to position in variable 'pos'
-   //   delay(15);  //waits 15ms for the servo to reach the position
-   // }
-
-    //delay(15000); //15000 = 15 seconds
+  if(active == 1){
+    motor.attach(7);  //re-attach the servo to pin #7
+    motor.write(180); // telling the motor to rotate continously
+    delay(1000);  // motor will continue rotating for 2 seconds  
+    motor.detach(); // telling the motor to stop
+    delay(15000);  // motor will remained stopped for 7 seconds 
+    motor.attach(7);  //re-attach the servo to pin #7
+    motor.write(0); // telling the motor to rotate continously
+    delay(1000);  // motor will continue rotating for 2 seconds    
+    active = 0;
     
- //   for (pos = 180; pos >= 0; pos += 1){ //goes from 0 degrees to 350 degrees
-   //   motor.write(pos); //tells servo to go to position in variable 'pos'
-   //   delay(15);  //waits 15ms for the servo to reach the position    
-   // }
-
- // }
-
+  }else{
+    active = 0;
+    motor.detach();
+    //delay(1000);
+  }
 }
+  
