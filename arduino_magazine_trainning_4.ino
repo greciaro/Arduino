5bc9db1066c3cfb4ff3b0f5ab22da8187f3a2cce
Grecia RO <56781403+greciaro@users.noreply.github.com>
2022-04-19 17:49:57 -0700

    Add files via upload

---
+++ arduino_magazine_trainning_4.ino
@@ -0,0 +1,55 @@
+//This program allows the door openning with:
+//(1) Manual activation 
+
+
+#include <Servo.h> //library to operate servo motors
+Servo motor; //create servo object to control a servo
+
+int Lswitch = 2;
+int statusSensor = 0; //variable to store if the switch was activated
+int statusDoor = 0; // variable to store of the door is open or closed. open = 1 closed = 0
+int count = 0; //variable to store how many times the door was activated
+
+unsigned long previousMillis = 0;
+int interval=12000;
+
+
+void setup() {
+  Serial.begin(9600);  //This value of bits per second is by default
+  motor.attach(7); //attaches the servo on pin 7 to the servo object
+  pinMode(Lswitch,INPUT_PULLUP);
+}
+
+void loop() {
+  
+   motor.detach();
+  if (digitalRead(Lswitch) == LOW){ //"LOW" means that the switch has been activated
+    statusSensor = 1;
+  }
+
+  if (statusSensor == 1 && statusDoor == 0)
+  {        
+    count = count+1;
+    Serial.println(count);
+    //Serial.print("\t");
+    motor.attach(7);  //re-attach the servo to pin #7
+    motor.write(0); // telling the motor to rotate continously (180 is full velocity in one direction, 0 full velocity in the other direction, 90 no movement)
+    delay(600);  // motor will continue rotating for 0.6 seconds
+    motor.detach(); // telling the motor to stop
+    statusDoor = 1;
+    statusSensor = 0;
+    Serial.println("Door open");
+  }
+
+    if (statusSensor == 1 && statusDoor == 1)
+  {        
+    motor.attach(7);  //re-attach the servo to pin #7
+    motor.write(180); // telling the motor to rotate continously (180 is full velocity in one direction, 0 full velocity in the other direction, 90 no movement)
+    delay(600);  // motor will continue rotating for 0.6 seconds
+    motor.detach(); // telling the motor to stop
+    statusDoor = 0;
+    statusSensor = 0;
+    Serial.println("Door closed");
+  }
+    
+}
