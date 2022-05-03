82e4b75e5c7a1c3b544fe3bb8545b807030e9d54
Grecia RO <56781403+greciaro@users.noreply.github.com>
2022-05-03 10:34:59 -0700

    Add files via upload

--- arduino_FR3.ino
+++ arduino_FR3.ino
@@ -7,12 +7,10 @@ Servo motor; //create servo object to control a servo
 
 int IRSensor = 2; // connect ir sensor to arduino pin 2 
 int count = 0; //variable to store how many times the door was activated
-int active = 0; //variable to store if the switch was activated
-int sensoractive = 0;
-int statusSensor = 0;
-int IR_out = LOW;
-
-boolean state = false;
+int sensoractive = 0;//variable to store if the switch was activated
+int number1 = 0; //intital state of the sensor
+int number2 = 0; //following state of the sensor
+int state = 0;
 
 void setup() {
   Serial.begin(9600);  //This value of bits per second is by default
@@ -22,52 +20,26 @@ void setup() {
 
 void loop() {
   motor.detach();
-
-//  void loop() {
-//  IR1_out = digitalRead(IR1);
-//  IR2_out = digitalRead(IR2);
-//  if(IR1_out == LOW) {
-//    increaseAndDisplay();
-//    delay(500);
-//  }
-//  if(IR2_out == LOW) {
-//    decreaseAndDisplay();
-//    delay(500);
-//  } 
-//  delay(100);
-//}
-
+  number1 = digitalRead (IRSensor);
+  delay(100);
+  number2 = digitalRead (IRSensor);
   
-  Serial.println(digitalRead(IRSensor)); 
-  
-//  if (IR_out == HIGH);
-//  {
-//    count++;
-//    state = false;
-//    Serial.print("Count: ");  
-//    Serial.println(count);  
-//    delay(100);
-//  }
-
-  if (digitalRead (IRSensor))
-  {
-    state = true;
-    delay(100);
+  if ((number1 - number2) == 1){
+    state = 1;
   }
   
-  if (sensoractive == 1)
-  {
-    sensoractive = 0;
-    delay(200);
-    active = active + 1;
-    delay(200);
-    Serial.println(active);
-    statusSensor = 0;
+  if (state == 1){
+    sensoractive++;
+    Serial.print("sensor activation: ");  
+    Serial.println(sensoractive);
+    state = 0;
+    number1 = 0;
+    number1 = 0;
   }
 
-  if (active == 3)
+  if (sensoractive == 3)
   {
-    count = count+1;
+    count++;
     Serial.print("openning #: ");
     Serial.println(count);
     motor.attach(7);  //re-attach the servo to pin #7
@@ -77,9 +49,8 @@ void loop() {
     delay(7000);  // motor will remained stopped for 7 seconds
     motor.attach(7);  //re-attach the servo to pin #7
     motor.write(180); // telling the motor to rotate continously
-    delay(505);  // motor will continue rotating for 0.505 seconds
+    delay(500);  // motor will continue rotating for 0.5 seconds
     motor.detach(); // telling the motor to stop
-    statusSensor = 0;
-    active = 0;
+    sensoractive = 0;
   } 
 }
