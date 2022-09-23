e0ac41cb687a01a911e738c11ad13b186bb5ca62
Grecia RO <56781403+greciaro@users.noreply.github.com>
2022-09-23 09:57:21 -0700

    Add files via upload

--- arduino_FR1.ino
+++ arduino_FR1.ino
@@ -8,7 +8,7 @@ Servo motor; //create servo object to control a servo
 int IRSensor = 2; // connect ir sensor to arduino pin 2 
 int count = 0; //variable to store how many times the door was activated
 int active = 0; //variable to store if the switch was activated
-
+unsigned long time;
 
 void setup() {
   Serial.begin(9600);  //This value of bits per second is by default
@@ -24,14 +24,17 @@ void loop() {
   {
     count = count+1;
     Serial.println(count);
+    Serial.print("Time passed (s):");
+    time = millis();
+    Serial.println(time/1000);
     motor.attach(7);  //re-attach the servo to pin #7
     motor.write(0); // telling the motor to rotate continously (180 is full velocity in one direction, 0 full velocity in the other direction, 90 no movement)
-    delay(500);  // motor will continue rotating for 0.5 seconds
+    delay(600);  // motor will continue rotating for 0.6 seconds
     motor.detach(); // telling the motor to stop
-    delay(7000);  // motor will remained stopped for 7 seconds
+    delay(3000);  // motor will remained stopped for 3 seconds
     motor.attach(7);  //re-attach the servo to pin #7
     motor.write(180); // telling the motor to rotate continously
-    delay(500);  // motor will continue rotating for 0.5 seconds
+    delay(600);  // motor will continue rotating for 0.6 seconds
     motor.detach(); // telling the motor to stop
     statusSensor = 0;
   } 
